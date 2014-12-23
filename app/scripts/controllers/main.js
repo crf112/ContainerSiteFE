7aded8ac9c39921a32f6f481111448511698717f
Tim Collinson <trcollinson@gmail.com>
2014-12-22 23:22:43 -0700

    Initial Commit of Angular Project.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name containerSiteFeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the containerSiteFeApp
+ */
+angular.module('containerSiteFeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
